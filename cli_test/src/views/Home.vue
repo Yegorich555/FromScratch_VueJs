93faf517ae1973a1aa538e882a94ce4f3ef6f942
Yegor G <yegorich_555@mail.ru>
2019-07-04 15:46:22 +0300

    Styles. Add css-modules architecture

--- cli_test/src/views/Home.vue
+++ cli_test/src/views/Home.vue
@@ -1,17 +1,3 @@
-<style module>
-.testClass {
-  font-weight: bold;
-  color: red !important;
-}
-</style>
-
-<template>
-  <div :class="$style.testClass">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
-  </div>
-</template>
-
 <script>
 // @ is an alias to /src
 import HelloWorld from "@/components/HelloWorld.vue";
@@ -23,3 +9,38 @@ export default {
   }
 };
 </script>
+
+<template>
+  <div>
+    <img alt="Vue logo" src="../assets/logo.png" />
+    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <!-- intellisense for named style module doesn't work: typing [$a.] is't show the popup despite on $style is. But [$style] works only for single style module -->
+    <div :class="$a.blue">Test splitted css</div>
+    <div>{{ $a }}</div>
+    <br />
+    <div :class="$b.blueSass">Test splitted scss</div>
+    <div>{{ $b }}</div>
+  </div>
+</template>
+
+<style module="$a" src="./myStyle.module.css">
+/*
+  .test-class doesn't join with css - it's replaced by css;
+  you can import css to module only via [src] attr
+ */
+.test-class-css {
+  color: white;
+}
+</style>
+
+<style module="$b" lang="scss">
+/*
+  you can import scss and extend style in single module;
+  if you can just one <style module> you can use it without names - in this case VSCode intellisense works fine
+*/
+@import "./myStyle.module.scss";
+
+.test-class-scss {
+  color: white;
+}
+</style>
