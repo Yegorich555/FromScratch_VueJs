181d9b505c56c19e60b6eae2ac15bbd2d9bc7598
Yegor G <yegorich_555@mail.ru>
2019-07-08 09:23:43 +0300

    Styles. Return 2nd bug between css-normalize and vue

--- cli_test/src/main.js
+++ cli_test/src/main.js
@@ -1,3 +1,4 @@
+import "@csstools/normalize.css"; //such import creates chunk-vendors.[hash].css that is placed after app.[hash].css in dist/index.html
 import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
